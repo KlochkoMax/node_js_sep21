05c38d8aa7c219c6a6ca55c61493e9e45f8f4b05
MaxymKlochko <klo4komax@gmail.com>
2022-02-15 22:33:20 +0300

    aawd

--- hw1/app.js
+++ hw1/app.js
@@ -24,36 +24,31 @@ let inPerson = [
     {name: 'Olena', year: '2013', city: 'Poltava'},
 ];
 
-    // fs.writeFile(path.join(__dirname,'main', 'inPerson', 'inPerson.txt'),
-    //     `NAME: ${inPerson[0].name}\n YEAR: ${inPerson[0].year}\n CITY: ${inPerson[0].city}`,
-    //     (err)=>{
-    //     if (err){
-    //         console.log(err)
-    //         throw err
-    //     }else
-    //         fs.writeFile(path.join(__dirname,'main', 'inPerson', 'inPerson.txt'),
-    //             `NAME: ${inPerson[1].name}\n YEAR: ${inPerson[1].name}\n CITY: ${inPerson[1].city}`,
-    //             (err)=>{
-    //                 if (err){
-    //                     console.log(err)
-    //                     throw err
-    //                 }
-    //             })
-    //
-    //     })
+// for (let i = 0; i < inPerson.length; i++){
+//         console.log([inPerson[i]])
+//         fs.writeFile(path.join(__dirname,'main', 'inPerson', 'inPerson.txt'),
+//             `NAME: ${[inPerson[i].name]}\nYEAR: ${[inPerson[i].year]}\nCITY: ${[inPerson[i].city]}\n`,
+//             {flag : 'a'},
+//             (err)=>{
+//                 if (err){
+//                     console.log(err)
+//                     throw err
+//                 }
+//             })
+// }
 
-for (let i = 0; i < inPerson.length; i++){
-        console.log([inPerson[i]])
-        fs.writeFile(path.join(__dirname,'main', 'inPerson', 'inPerson.txt'),
-            `NAME: ${[inPerson[i].name]}\nYEAR: ${[inPerson[i].year]}\nCITY: ${[inPerson[i].city]}\n`,
-            {flag : 'a'},
-            (err)=>{
-                if (err){
-                    console.log(err)
-                    throw err
-                }
-            })
-}
+// for (let i = 0; i < online.length; i++){
+//     console.log([online[i]])
+//     fs.writeFile(path.join(__dirname,'main', 'online', 'online.txt'),
+//         `NAME: ${[online[i].name]}\nYEAR: ${[online[i].year]}\nCITY: ${[online[i].city]}\n`,
+//         {flag : 'a'},
+//         (err)=>{
+//             if (err){
+//                 console.log(err)
+//                 throw err
+//             }
+//         })
+// }
 
 
 
