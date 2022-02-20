a1e6c3d1c6732d8a149700c49d5243d982ccbedf
MaxymKlochko <klo4komax@gmail.com>
2022-02-20 16:43:15 +0300

    hw2 done

--- hw2/app.js
+++ hw2/app.js
@@ -3,6 +3,7 @@ const {engine} = require('express-handlebars');
 const path = require('path');
 
 const users = [];
+let userInfo = [];
 
 const app = express();
 
@@ -14,9 +15,11 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
+
 app.get('/login', ( req , res ) => {
     res.render('login');
 })
+
 
 app.get('/users',( {query} , res) => {
     if (query.city || query.age) {
@@ -29,7 +32,7 @@ app.get('/users',( {query} , res) => {
         if (query.age){
             filteredArray = filteredArray.filter(userAge => userAge.age === query.age);
         }
-        console.log(filteredArray)
+        // console.log(filteredArray)
         res.render('users', {users:filteredArray});
         return
     }
@@ -37,8 +40,10 @@ app.get('/users',( {query} , res) => {
     res.render('users', {users})
 })
 
+
 app.post('/login', (req, res) => {
     newUser = req.body;
+    // console.log(newUser)
     let findEmail = users.map(user => user.email);
     if (findEmail.includes(newUser.email)){
         res.render('errorPage');
@@ -48,16 +53,49 @@ app.post('/login', (req, res) => {
     res.redirect('/users')
 })
 
-app.get('/users/:id', (req,res)=>{
-    let user = users.find(user => user.id === +req.params.id );
-    res.render('user/id', {user})
+
+app.get('/users/:id', ( req , res )=>{
+    userInfo = users.find(user => user.id === +req.params.id );
+    res.redirect('/user/id')
 })
+
+
+app.get('/user/id',(req,res)=>{
+    console.log(userInfo)
+    res.render('user/id', {userInfo})
+})
+
+
+app.get('/signIn',(req,res)=>{
+    res.render('signIn')
+})
+
+
+app.post('/signIn', (req, res)=>{
+     let userSearch = req.body;
+     userInfo = users.filter(user => (user.password === userSearch.password && user.email === userSearch.email))
+     if (userInfo.length){
+         res.redirect('/user/id');
+         return
+     }else
+      res.render('notFoundPage');
+})
+
 
 app.use((req, res)=>{
     res.render('notFoundPage')
 })
 
+
 app.listen(5200, ()=>{
     console.log('Server starts successful')
 })
+
+
+// Необхідно розширити ваше ДЗ:
+//     - додайте ендпоінт signIn який буде приймати email і password
+// і якщо все вірно то редірект на сторінку цього користувача
+//
+// * хто хоче складніше реалізуйте видалення користувача. Кнопка
+// повинна знаходитись на сторінці з інфою про одного юзера. Після видалення редірект на "/users"
 
